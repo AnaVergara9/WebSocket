4318a280bca1349eaa25c6fe3582d3344358910f
Sofia Vergara <ana.vergara@estudiantesunibague.edu.co>
2026-05-05 17:58:32 -0500

    Creación useEffect

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
 function App() {
+  //useState es un hook de React que nos permite ir actualizando lo de la pantalla sin necesidad de recargarla
+  const [socket, setSocket] = useState();
+
+  useEffect(() => {
+    // Cuando yo carge la pagina hace lo que meta ahí dentro, en este caso, se conecta al servidor de socket.io
+    const newSocket = io("localhost:3000");
+
+    return () => {
+      newSocket.disconnect();
+    }
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(socket) {
+      socket.emit("message", inputMessage)
+    }
+  }
+  
   return (
-    <div className="App">
-      <h1>Hola!</h1>
+    <div>
+      <form>
+        <input type="text" placeholder="Escribir el mensaje"/>
+        <button>Enviar</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
